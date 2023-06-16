5891f29a38f509bfa7ca0fd962843874d619be98
ali <112857919+Ali010042@users.noreply.github.com>
2023-06-16 10:07:50 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-
+// dit de de stukje php voor om css te gebruiken in mijn wordpress//
 add_action("wp_enqueue_scripts", "css");
 
 function css(){
-wp_enqueue_style("css1", get_template_directory_uri() . "/style.css");
+wp_enqueue_style("css1", get_template_directory_uri() . "./style.css");
 }
-
+// Dit is de functie voor mijn navbar om het de button voor "menu" te krijgen//
 function register_my_menus() {
     register_nav_menus(
       array(
@@ -15,3 +15,45 @@ function register_my_menus() {
      );
    }
    add_action( 'init', 'register_my_menus' );
+
+
+   function ti_custom_javascript() {
+
+
+
+    ?>
+
+    <!-- stukje javascript voor de quote-->
+        <script>
+      
+    // Array of e-sport quotes
+ const quotes = [
+  "In e-sport wordt niemand per ongeluk kampioen.",
+  "Hard werken en toewijding banen de weg naar e-sportsglorie.",
+  "E-sport is waar vaardigheid, strategie en vastberadenheid samenkomen.",
+  "De sensatie van overwinning in e-sport is ongeëvenaard.",
+  "E-sport is niet zomaar een spel; het is een manier van leven.",
+  "Succes in e-sport vereist teamwork en communicatie.",
+  "E-sport is meer dan alleen een hobby; het is een passie.",
+  "De grenzen verleggen en records verbreken is wat e-sport drijft.",
+  "In e-sport draait het om discipline en doorzettingsvermogen.",
+  "E-sport is een arena waar kampioenen worden geboren en legendes worden gemaakt."
+];
+    
+
+    // Funtie voor random quote
+    function generateQuote() {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const quoteElement = document.getElementById('quote');
+      quoteElement.textContent = quotes[randomIndex];
+    }
+
+    // add advent listeren naar de code zodat js kan zien wanneer je erop klikt
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', generateQuote);
+  </script>
+      
+    <?php
+}
+add_action('wp_head', 'ti_custom_javascript');
+
--- index.php
+++ index.php
@@ -4,17 +4,21 @@
     <meta name="viewport" content="width=device-width" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" href="style.css">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+     <title>TTV</title>
 
 </head>
 <body>
+  <!-- dit is de php code voor mijn nav bar-->
     <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
    
+    <!-- dit de de php code waar die de content ophaalt en in een div zit-->
  <div>
     <?php echo get_the_content(); ?>
  </div>
  
-
+  
 </body>
 </html>
 
